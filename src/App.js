116332813273ca8ba75353d7f770b560995646b1
Evgeniy Belkovskiy <ebelkovskiy9@gmail.com>
2022-09-14 00:15:31 +0300

    Create counter logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={decrementCount}>- Минус</button>
+        <button className="plus" onClick={incrementCount}>Плюс +</button>
       </div>
     </div>
   );
